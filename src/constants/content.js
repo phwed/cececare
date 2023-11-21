14e63e4aaf166248bae501c4bdc1f60331b00810
gsomuah <85286525+gsomuah@users.noreply.github.com>
2023-11-21 16:42:48 +0000

    changes

--- src/constants/content.js
+++ src/constants/content.js
@@ -1,10 +1,10 @@
 export const content = {
   opening_hours: "Openning Hours: 9am - 5pm",
-  whatsapp: "https://wa.me/<number>",
-  phone: "tel:<number>",
-  email: "mailto:<email>",
-  book_an_appointment: "Book An Appointment",
-  download_link: "/dummy.pdf",
+  whatsapp: "https://wa.me/<+44 7950 377186>",
+  phone: "tel:<+44 7950 377186>",
+  email: "mailto:<info@cececare.com>",
+  book_an_appointment: "Download form",
+  download_link: "/application form.pdf",
   header: {
     home: "Home",
     about: "About Us",
@@ -13,12 +13,12 @@ export const content = {
   },
   footer: {
     leading: "Give Your Loved Ones Quality Care You Can Trust",
-    copyright: "©Copyright 2023. All Rights Reserved. Stellar Gh",
+    copyright: "©Copyright 2023. All Rights Reserved. Soluble tech",
     contact_descriptoin:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-    address: "Address",
-    phone: "Phone",
-    email: "Email",
+      "United Kindom.",
+    address: "17 West Road,  Lancaster,  LA1 5PG",
+    phone: "+44 7950 377186",
+    email: "info@cececare.com",
     socials: {
       facebook: "https://www.facebook.com/",
       twitter: "https://www.twitter.com/",
@@ -35,9 +35,9 @@ export const content = {
   banner: {
     image:
       "https://s3-alpha-sig.figma.com/img/e185/1ea7/961dcb0b148e719c2b333c6d7c24f9d3?Expires=1698624000&Signature=edp6UVN4wadNp5fIgI~lP5682mY0wzEz8YLo7L3H5PLfSNiYxhM32wWyLf2dKh0OacXqKOqBsglEqY-5N~a3~EilmHRatpDW~j9cq~Ny4QmlaRsu71AyaQFomLpDbdtcp0CAoD9HpUPXvZ1T24raShCc-B3fnBxlxGUEzQM8REdCheC5AYp2GyV8Q3UD2uikOKeY-Z0zcFXYTT6oHtOcDKTiEmbx9cFZhOCF9vcmVnTvQxPPi9vZSgXsKGTqwV4mKq7O4m4TjvvY6YzkAk7tW2Cvmgkl-oVmcISXJ6KpyVlUAJ1G7DViT4bptRKlAidhME7M7R1fgaHYeiNgzxYTug__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4",
-    title: "Enriching Happiness",
+    title: "Providing quality care that transforms lives",
     subtitle:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec.ullamcorper mattis, pulvinar dapibus leo.",
+      "",
     learn_more: "Learn More",
     services: [
       {
@@ -55,10 +55,17 @@ export const content = {
     ]
   },
   welcome: {
-    title: "Welcome to CeCe Care Services",
-    header: "See & Experience A New Perspective Of Care",
-    subtitle:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus vel dolor felis. In ligula purus, imperdiet ac est nec, tempus varius nisl. Integer et venenatis odio. Fusce enim lectus, gravida ut justo sed, molestie fermentum libero.",
+    title: "ABOUT US",
+    about_us:
+    "At CeCe Care Services, we think that kindness, commitment, and steadfast support can make a difference in the lives of people in all walks of life. Our goal is to improve the lives of our clients by offering them outstanding care services, whether they are elderly people reaching their prime, young children just starting out in the world, or disabled people pursuing self-determination and empowerment." +
+    "We have developed a care service that is as varied as the needs of our clients because we have a strong commitment to promoting well-being and improving quality of life. Every person in our care will get exceptional support and companionship from our compassionate and skilled team of carers." +
+    "Our customized approach is what distinguishes CeCe Care Services.",
+    mission: "Mission",
+    vision: "Vision",
+    mission_statement:
+      "CeCe Care Services prioritizes clients' physical, emotional, and social well-being, providing individualized, empathetic, and comprehensive care through teamwork, creativity, and a strong sense of duty.",
+    vision_statement:
+      "We aim to create a society where every individual with a disability, children, and adults enjoys dignity, compassion, and limitless opportunities, fostering empathy, inclusion, and support.",
     points: [
       "Nulla mollis tristique blandi",
       "Mauris eu ante euismod",
@@ -69,17 +76,16 @@ export const content = {
     service_two: "Safe Solutions For Health"
   },
   care: {
-    title: "Types of Care",
-    header: "Provising Quality Care Services",
+    title: "OUR SERVICES",
     subtitle:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus vel dolor felis. In ligula purus, imperdiet ac est nec, tempus varius nisl. Integer et venenatis odio. Fusce enim lectus, gravida ut justo sed.",
+      "we offfer a wide range of services which includes.",
     service: [
       {
         image:
           "https://tykit.rometheme.pro/seniorsy/wp-content/uploads/sites/78/2022/08/senior-couple-at-home-with-man-talking-to-female-n-2022-04-04-21-32-57-utc.jpg",
         title: "Adult Care",
         subtitle:
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec.",
+          "For adults who may require assistance with activities of daily living (ADL), such as meal preparation, medication management, companionship, or transportation, we offer a range of in-home care services. This can include options for both short-term and long-term care.",
         link: "/services/adult-care"
       },
       {
@@ -87,34 +93,48 @@ export const content = {
           "https://tykit.rometheme.pro/seniorsy/wp-content/uploads/sites/78/2022/08/female-care-worker-in-uniform-bringing-meal-on-tra-2022-04-04-21-27-31-utc.jpg",
         title: "Child Care",
         subtitle:
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec.",
-        link: "/services/child-care"
+          "Parents can rely on our Services for high-quality childcare solutions. This includes nurturing and safe environments, educational support, and activities tailored to children's developmental stages and needs."
       },
       {
         image:
           "https://tykit.rometheme.pro/seniorsy/wp-content/uploads/sites/78/2022/08/female-doctor-examining-a-senior-woman-2021-08-26-12-07-48-utc.jpg",
         title: "People With Disabilities",
         subtitle:
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec.",
+          "we are dedicated to offering specialized care for individuals with disabilities. This may encompass physical disabilities, developmental disorders, or cognitive impairments. The services may include personal care, therapy support, and creating inclusive activities for social engagement..",
         link: "/services/pwd-care"
-      }
+      },
+      {
+        image:
+          "https://tykit.rometheme.pro/seniorsy/wp-content/uploads/sites/78/2022/08/female-care-worker-in-uniform-bringing-meal-on-tra-2022-04-04-21-27-31-utc.jpg",
+        title: "Skilled Caregivers",
+        subtitle:
+          "Parents caour success hinges on the expertise and compassion of its caregivers. They should be well-trained, certified, and have a genuine passion for providing care. Regular training and ongoing professional development are essential to maintain high-quality standardsn rely on our Services for high-quality childcare solutions. This includes nurturing and safe environments, educational support, and activities tailored to children's developmental stages and needs."
+      },
+
+      {
+        image:
+          "https://tykit.rometheme.pro/seniorsy/wp-content/uploads/sites/78/2022/08/female-care-worker-in-uniform-bringing-meal-on-tra-2022-04-04-21-27-31-utc.jpg",
+        title: "Respite Care",
+        subtitle:
+          "we recognize the challenges that family caregivers often face. To offer relief and support, they can provide respite care, allowing primary caregivers to take a break while ensuring that their loved ones are well cared for.Parents caour success hinges on the expertise and compassion of its caregivers. ."
+      },
+      
     ]
   },
   why: {
-    title: "Why Choose CeCe Care Services",
-    header: "Our Heart Is Full Of Compassion For Your Care",
+     title: "WHY US",
     subtitle:
-      "Nullam porttitor metus justo, sit amet luctus arcu consequat a. Fusce iaculis leo a augue malesuada pharetra. Fusce porta porttitor odio vel placerat. Etiam vitae lacus finibus,",
+      "Our commitment lies in delivering outstanding in-home care services that put our clients' comfort and well-being first. Our dedication is motivated by a strong conviction in the value of individualized care, in which each person is shown respect, decency, and compassion. Topmost of our priorities are our clients :",
     points: [
-      "Suspendisse porta porttitor rhoncus",
-      "Etiam dictum condimentum metus",
-      "Proin blandit sodales diam facilisis",
-      "Class aptent taciti sociosqu ad"
+      "Safety",
+      "Independence",
+      "Happiness",
+      "Comfort"
     ],
     call: "Clarify Your Queries Call Us Now",
-    phone: "0209130483",
+    phone: "+44 7950 377186",
     phoneLink: "tel:<number>",
-    slogan: "The Patient Is First Priority In Our Passion"
+    slogan: "The client Is First Priority In Our Passion"
   },
 
   testimonials: {
@@ -122,7 +142,7 @@ export const content = {
       "https://s3-alpha-sig.figma.com/img/fc02/3707/ec95b73e0797c0837ee904667b1d5b97?Expires=1698624000&Signature=hH~e0GMSada-MyQANm62EZTscOC7~kKenoPCJD7-VPO9ALBC9Cmlab322NxJNSoMoQrIZGI~jRMFWL2j~udNk~FpF~WSGZyQFBrCI2QiQdEBVCOnKzHDp1-5LWfQzgv1fmsNznHBIS6imF89lqdOwPnziKjuYdfXpP1xLmFegESU4ux19FBXKn~fWhsw-6zvEnPLwP~ceHzLbBYwHq5rHzOcHNPKsRI4DhLWvqHoq0yhdsTqjSXzZi5wQFwQi2gspqpFuO9tNsFXoHC~p5O2G1gCXiavzE5OC1r15FgUbOoLZUhTwP3kmhNZ5Xfjs8I8bh6JYWiYBi~C8QsU44WPlg__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4",
     title: "We Care With Compassion",
     subtitle:
-      "Morbi vehicula risus scelerisque justo fermentum volutpat. Praesent lobortis nunc nec ligula elementum, sed accumsan ligula facilisis. Class aptent taciti sociosqu ad litora torquent per conubia nostra.",
+      "We take pride in our ability to form meaningful connections, fostering a sense of community and companionship for those under our care. With our comprehensive range of services and unwavering commitment to excellence, we strive to make a positive difference in the lives of our clients each day.",
     left: {
       title: "What They Say ?",
       header: "Love To Serve You, With Compassionate Care",
@@ -132,24 +152,24 @@ export const content = {
     right: [
       {
         comment:
-          "Sed congue mollis orci et tempus. Etiam semper elit id turpis ornare posuere a sit amet elit. Nulla facilisi. Nullam augue lectus, lobortis et risus id, ornare porttitor arcu. Pellentesque habitant morbi tristique senectus et netus.",
-        name: "John Doe",
-        position: "CEO, Company",
+          "testimony 3.",
+        name: "Ezekiel Addison-Otoo",
+        position: "CEO, Besco",
         image: "https://i.pravatar.cc/150?u=firstimage"
       },
 
       {
         comment:
-          "Sed congue mollis orci et tempus. Etiam semper elit id turpis ornare posuere a sit amet elit. Nulla facilisi. Nullam augue lectus, lobortis et risus id, ornare porttitor arcu. Pellentesque habitant morbi tristique senectus et netus.",
-        name: "John Doe",
+          "testimony2, .",
+        name: "Gladys Addison-Otoo",
         position: "CEO, Company",
         image: "https://i.pravatar.cc/150?u=secondimage"
       },
       {
         comment:
-          "Sed congue mollis orci et tempus. Etiam semper elit id turpis ornare posuere a sit amet elit. Nulla facilisi. Nullam augue lectus, lobortis et risus id, ornare porttitor arcu. Pellentesque habitant morbi tristique senectus et netus.",
-        name: "John Doe",
-        position: "CEO, Company",
+          "testimony 1, .",
+        name: "Godfred Somuah",
+        position: "CEO, Soluble tech",
         image: "https://i.pravatar.cc/150?u=thirdimage"
       }
     ]
@@ -162,17 +182,17 @@ export const content = {
       {
         title: "Book Your Appointment",
         subtitle:
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo."
+          "appointment text."
       },
       {
         title: "In Person Counselling",
         subtitle:
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo."
+          "conselling text."
       },
       {
         title: "Weekly Followup",
         subtitle:
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo."
+          "follow up text."
       }
     ]
   }
