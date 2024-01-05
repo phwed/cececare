33402df506bab094fd42c6053c234557688a135f
Phred <adufred7@gmail.com>
2024-01-05 13:23:22 +0000

    final edits

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,17 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
-
+// Import /static for a static site
+import vercelStatic from "@astrojs/vercel/static";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  output: "static",
+  adapter: vercelStatic({
+    webAnalytics: {
+      enabled: true
+    },
+    maxDuration: 8
+  }),
   integrations: [react(), tailwind()]
 });
